6ce078fdb5740b428bf2d461c305be799de6e65d
Stellia07 <83445251+Stellia07@users.noreply.github.com>
2024-03-02 13:11:43 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI, Request, HTTPException
+app = FastAPI()
+
+@app.get("/") 
+async def read_main(token: str = None):
+   if token != "jayson":
+      raise HTTPException(status_code=401, detail="Unauthorized")
+   return {"message": "Hello, World!"}
